538898df72d302b1f311f13d4dd39595946cf527
Mychal Hall <hallmychal@gmail.com>
2019-02-05 23:25:41 -0600

    Article Done, thanks random period

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,6 @@
 
 toggleMenu = () => (menu.classList.toggle('menu--open'));
-console.log(toggleMenu)
-
 const menu = document.querySelector('.menu');
-console.log(menu);
-
 const menuButton = document.querySelector('.menu-button');
-
 menuButton.addEventListener('click', toggleMenu);
-console.log(menuButton);
+
